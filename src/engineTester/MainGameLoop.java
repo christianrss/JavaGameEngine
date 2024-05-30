d23b88e464aa654a44e9794bb4a29a66e1aa4a21
Christian <christianrssdev@gmail.com>
2024-05-30 13:49:15 -0300

    OBJ Loader

--- src/engineTester/MainGameLoop.java
+++ src/engineTester/MainGameLoop.java
@@ -5,6 +5,7 @@ import org.lwjgl.util.vector.Vector3f;
 
 import entities.Camera;
 import entities.Entity;
+import entities.OBJLoader;
 import models.RawModel;
 import models.TexturedModel;
 import renderEngine.DisplayManager;
@@ -22,35 +23,17 @@ public class MainGameLoop {
 		Loader loader = new Loader();
 		StaticShader shader = new StaticShader();
 		Renderer renderer = new Renderer(shader);
-		float[] vertices = {
-			// Left bottom triangle
-			 -0.5f,  0.5f, 0f, // v0
-			 -0.5f, -0.5f, 0f, // v1
-			  0.5f, -0.5f, 0f, // v2
-			  0.5f,  0.5f, 0f, // v3
-		};
 		
-		int[] indices = {
-			0,1,3,		// Top left triangle (V0,V1,V3)
-			3,1,2		// Bottom right triangle (V3,V1,V2)
-		};
-		
-		float [] textureCoords = {
-				0,0, // V0
-				0,1, // V1
-				1,1, // V2
-				1,0, // V3 
-		};
-		
-		RawModel model = loader.loadToVAO(vertices,textureCoords,indices);
-		TexturedModel staticModel = new TexturedModel(model, new ModelTexture(loader.loadTexture("image")));
+		RawModel model = OBJLoader.loadObjModel("stall", loader);
+		TexturedModel staticModel = new TexturedModel(model, new ModelTexture(loader.loadTexture("stallTexture")));
 		//TexturedModel staticModel = new TexturedModel(model, texture);
 		
-		Entity entity = new Entity(staticModel, new Vector3f(0,0,-1),0,0,0,1);
+		Entity entity = new Entity(staticModel, new Vector3f(0,0,-50),0,0,0,1);
 		Camera camera = new Camera();
 		
 		while (!Display.isCloseRequested()) {
-			entity.increasePosition(0, 0, -0.1f);
+			//entity.increasePosition(0, 0, -0.1f);
+			entity.increaseRotation(0, 1, 0);
 			camera.move();
 			renderer.prepare();
 			shader.start();
