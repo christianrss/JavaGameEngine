ae1c9e4afaee18c8c4406351f17eacfa87736c55
Christian <christianrssdev@gmail.com>
2024-06-01 10:50:55 -0300

    Fog

--- src/engineTester/MainGameLoop.java
+++ src/engineTester/MainGameLoop.java
@@ -38,12 +38,18 @@ public class MainGameLoop {
 		TexturedModel fern = new TexturedModel(OBJLoader.loadObjModel("fern", loader), new ModelTexture(loader.loadTexture("fern")));
 		fern.getTexture().setHasTransparency(true);
 		
+		TexturedModel flower = new TexturedModel(OBJLoader.loadObjModel("grassModel", loader), new ModelTexture(loader.loadTexture("flower")));
+		flower.getTexture().setHasTransparency(true);
+		flower.getTexture().setUseFakeLighting(true);
+
+		
 		List<Entity> entities = new ArrayList<Entity>();
 		Random random = new Random();
 		for(int i = 0; i < 500; i++) {
 			entities.add(new Entity(staticModel, new Vector3f(random.nextFloat() * 800 - 400,0,random.nextFloat() * -600),0,0,0,3));
 			entities.add(new Entity(grass, new Vector3f(random.nextFloat() * 800 - 400,0,random.nextFloat() * -600),0,0,0,1));
 			entities.add(new Entity(fern, new Vector3f(random.nextFloat() * 800 - 400,0,random.nextFloat() * -600),0,0,0,0.6f));
+			entities.add(new Entity(flower, new Vector3f(random.nextFloat() * 800 - 400,0,random.nextFloat() * -600),0,0,0,1));
 		}
 		
 		texture.setShineDamper(10);
